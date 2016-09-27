f2eae2b2b48f8481c95db5c9b39eec8e87f4f4cb
Si Jobling <simon.jobling@gmail.com>
2016-09-27 08:52:08 +0100

    String spec and src as separates

--- leap-year/jasmine/spec/StringCalculator_Spec.js
+++ leap-year/jasmine/spec/StringCalculator_Spec.js
@@ -40,24 +40,3 @@ describe("String calculator", function() {
       expect(result).toBe(5);
   })    
 });
-
-function Add(input) {
-    if(input === '') {
-        return 0;
-    }
-
-    var found = input.match(/\/\/.+\n/);
-    var delimiter = /[\,,\n]{1}/;
-    if(found && found.length!==0) {
-        var configLine = found[0];
-        var delimiterPosition = 2;
-        delimiter = configLine.substring(delimiterPosition, configLine.length - 1);
-        input = input.replace(configLine, '');
-    }
-    var numbers = input.split(delimiter);        
-    var total = 0;
-    numbers.forEach(function(num,i) {
-        total += parseInt(num,10);
-    });
-    return total;
-}
