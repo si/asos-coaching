0bbe2f23ef6e0f9f99a6ca35af9ab256d997d09e
Si Jobling <simon.jobling@gmail.com>
2016-11-09 14:24:46 +0000

    Add clock stub to ATM unit test

--- leap-year/jasmine/spec/ATM_Unit_Spec.js
+++ leap-year/jasmine/spec/ATM_Unit_Spec.js
@@ -2,6 +2,8 @@ describe('[UNIT] ATM', function() {
   var transactionHistory;
   var statement;
   var printer;
+  var clock;  
+  var stubDate;
 
   beforeEach(function() {
     transactionHistory = {
@@ -14,6 +16,12 @@ describe('[UNIT] ATM', function() {
     spyOn(transactionHistory, 'addTransaction');
     spyOn(transactionHistory, 'getTransactions');
     spyOn(statement, 'getStatement');
+
+    clock = {
+      getDate: function() {
+        return stubDate.getFullYear() + '-' + stubDate.getMonth() + stubDate.getDay();  
+      }
+    }
 
     printer = {
       print: function() {}
@@ -33,4 +41,18 @@ describe('[UNIT] ATM', function() {
     expect(transactionHistory.getTransactions).toHaveBeenCalled();
     expect(statement.getStatement).toHaveBeenCalled();
   });
+});
+
+describe('[UNIT] Transaction History', function() {
+  it('should register deposit of 1000', function() {
+    var transactionHistory = new TransactionHistory();
+    transactionHistory.addTransaction(1000, 'Deposit', new Date(2012, 01, 10));
+    var transactions = transactionHistory.getTransactions();
+    expect(transactions.length).toBe(1);
+    expect(transactions[0].amount).toBe(1000);
+    expect(transactions[0].type).toBe('Deposit');
+    expect(transactions[0].date.getFullYear()).toBe('2016');
+    expect(transactions[0].date.getMonth()).toBe('ß01');
+    expect(transactions[0].date.getDay()).toBe('10');ß
+  })
 });
